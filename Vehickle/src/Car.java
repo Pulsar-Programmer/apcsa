6841db61e1442ca877011e73ee63f9a9c8823ceb
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-01-31 14:06:49 -0500

    Delete Vehickle directory

--- Vehickle/src/Car.java
+++
@@ -1,5 +0,0 @@
-public class Car extends Vehicle {
-    public double getValue(){
-        return super.getValue() + 1000 * condition;
-    }
-}
--- Vehickle/src/Helicopter.java
+++
@@ -1,7 +0,0 @@
-public class Helicopter extends Vehicle {
-    private int maxPassengerWeight;
-
-    public double getValue(){
-        return super.getValue() + 5000 * (int)(maxPassengerWeight / 1000);
-    }
-}
