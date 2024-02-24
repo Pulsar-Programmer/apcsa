a6ff71e6ada36c9afa0f4b1e691540cf0aad8523
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-02-23 20:37:02 -0500

    Huzzazah!

--- HanoiAnnoi/src/Driver.java
+++ HanoiAnnoi/src/Driver.java
@@ -1,14 +1,19 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+///Note: Triple comments are intended to be read.
+//Note: Double comments give no meaningful information.
+
 public class Driver {
     public static void main(String[] args) throws Exception {
+        ///We ready our scanner.
         var scanner = new Scanner(System.in);
-        //make it sleep
-        //make it clear the console
+        ///We create our game loop.
         while(true){
+            ///We create our tower.
             var my_tower = new Towers();
 
+            ///We query for a tower size.
             System.out.println("How would you like your tower? Enter a number.");
             final var size = scanner.nextInt(); scanner.nextLine();
             if(size < 1){
@@ -17,16 +22,23 @@ public class Driver {
             }
             my_tower.setTower(size);
 
+            ///We query whether we should involve the solution or not.
             System.out.println("Do you want me to show you the solution before you attempt it? (enter `true` or `false`)");
             final var for_ai = scanner.nextBoolean(); scanner.nextLine();
-            
+
+            ///We first keep track of the type of move next to do.
             var aimove = 0;
+            ///While my_tower is not solved, we continue.
             while(!my_tower.isSolved()){
+                ///We clear the console and display our tower.
                 clear_console();
                 System.out.println(my_tower);
                 
+                ///If we must solve it, lets do it, otherwise the user is free to.
                 if(for_ai){
+                    ///We sleep so the user has a chance to see the animation.
                     Thread.sleep(1000);
+                    ///We measure the move. Based on which time, we roll the makeMove in the special wikipedia config.
                     switch (aimove) {
                         case 0:
                             my_tower.makeMove(0, 1);
@@ -40,12 +52,16 @@ public class Driver {
                         default:
                             break;
                     }
+                    ///Each time, we increment the move.
                     aimove += 1;
+                    ///Then we cap it my circling it at 3 with only three states.
                     aimove %= 3;
                 } else {
+                    ///We query the two values, one after the other.
                     System.out.println("Which two values do you want to move? Enter the first followed by the second.");
                     final var one = scanner.nextInt(); scanner.nextLine();
                     final var two = scanner.nextInt(); scanner.nextLine();
+                    ///If the move is invalid, we must address it.
                     if(!my_tower.makeMove(one, two)){
                         System.out.println("The move you attempted to make was invalid! Please try again!");
                         Thread.sleep(1000);
@@ -53,11 +69,14 @@ public class Driver {
                     }
                 }
             }
+            ///We clear the console and print out the tower.
             clear_console();
             System.out.println(my_tower);
+            ///If the user solved it, say good job!
             if(!for_ai){
                 System.out.println("Great job! You've solved it!");
             }
+            ///Request a re-run.
             System.out.println("Care to try again? (enter true or false)");
             if(!scanner.nextBoolean()) break;
         }
@@ -67,6 +86,7 @@ public class Driver {
     public static void clear_console(){
         // System.out.print("\033[H\033[2J");
         // System.out.flush();
+        ///Print 20 lines of empty returns.
         for(var i = 0; i < 20; i+=1) System.out.println();
     }
 }
--- HanoiAnnoi/src/Towers.java
+++ HanoiAnnoi/src/Towers.java
@@ -5,20 +5,26 @@ public class Towers {
     private ArrayList<Integer> middle;
     private ArrayList<Integer> right;
 
-    private static double clamp(double value, double min, double max) {
-        return Math.max(min, Math.min(max, value));
-    }
+    //This is a utility function to clamp something between two values.
+    // private static double clamp(double value, double min, double max) {
+    //     return Math.max(min, Math.min(max, value));
+    // }
 
+    ///This finds the max length of the three towers.
     private int max_len(){
         return Math.max(Math.max(left.size(), middle.size()), right.size());
     }
 
     @Override
     public String toString() {
+        ///We make our string.
         String string = "- - -\n";
+        ///Append the indices.
         string += "0 1 2\n";
 
+        ///We find the max length.
         final var len = max_len();
+        ///We iterate over each and then prepend each part.
         for(var i = 0; i < len; i += 1){
             final var leftc = get_as_str_or_space(left, i);
             final var midc = get_as_str_or_space(middle, i);
@@ -29,11 +35,14 @@ public class Towers {
     }
     
     public boolean makeMove(int one, int two){
+        ///If the move is invalid, we cannot preform it.
         if(one % 3 != one || two % 3 != two)
             return false;
+        ///We get the reference to the corresponding array which we need.
         var a1 = corresponding_array(one);
         var a2 = corresponding_array(two);
         
+        ///We find which move is legal. If neither are, we cannot preform the move.
         if(assert_legality(a1, a2)){
             a2.add(a1.remove(a1.size()-1));
         } else if(assert_legality(a2, a1)) {
@@ -45,12 +54,16 @@ public class Towers {
     }
 
     private boolean assert_legality(ArrayList<Integer> from, ArrayList<Integer> to){
+        ///If you are drawing from an empty slot, you are immediately in the wrong.
         if(from.isEmpty()) return false;
+        ///If you are placing an item in an empty slot, you are totally fine.
         if(to.isEmpty()) return true;
+        ///If all this is fine, then we can safely peek at the values and determine the larger one to place.
         return peek(to) > peek(from);
     }
 
     private ArrayList<Integer> corresponding_array(int val){
+        ///We find the corresponding array. If there is an issue with this, we can just loop back with the modded value.
         switch (val) {
             case 0:
                 return left;
@@ -65,18 +78,25 @@ public class Towers {
 
     public boolean isSolved(){
         //each element must be one less than the one before it;
+        ///We first check to make sure that if the left or middle have any contents, we must return false for the config is not solved.
         if(!left.isEmpty() || !middle.isEmpty()){
             return false;
         }
+        ///We create a loop ranging from 0..(the len of the right peg - 1)
         for(var i = 0; i < right.size() - 1; i+=1){
+            ///We do this so that we can compare each element to make sure they are in order to avoid illegal mechanisms.
+            ///Indeed, we check if the lower elem is bigger than the higher. If not, we return false.
             if(right.get(i) <= right.get(i + 1)){
                 return false;
             }
         }
+        ///Otherwise we safely return true here.
         return true;
     }
 
     public static ArrayList<Integer> with_num(int num){
+        ///This is a very helpful factory function (I prefer them over constructors).
+        ///It creates a tower and fills up the necessary pegs up to the relevant num.
         var tower = new ArrayList<Integer>();
 
         for(var i = 0; i < num; i += 1){
@@ -87,44 +107,50 @@ public class Towers {
     }
 
     public void setTower(int num){
+        ///This sets the left tower while clearing the others.
         left = with_num(num);
         middle.clear();
         right.clear();
     }
 
     public void reset(){
+        ///This essentially sets the tower with the current amount of disks found.
         setTower(getCount());
     }
 
     public int getCount(){
+        ///In order to get the count, we must return the sum of all the lens of the vectors.
         return left.size() + middle.size() + right.size();
     }
 
     private static Integer peek(ArrayList<Integer> inner){
+        ///If it is empty, which should not happen, we just assing the largest number to our peek.
         if(inner.isEmpty()){
             return Integer.MAX_VALUE; //this should never be triggered
         }
+        ///Otherwise, we genuinely check what is inside.
         return inner.get(inner.size() - 1);
     }
 
-    private static Integer pop_or_zero(ArrayList<Integer> inner){
-        var size = inner.size();
-        if(size == 0){
-            return 0;
-        }
-        var get = inner.get(size - 1);
-        inner.remove(size - 1);
-        return get;
-    }
+    // private static Integer pop_or_zero(ArrayList<Integer> inner){
+    //     var size = inner.size();
+    //     if(size == 0){
+    //         return 0;
+    //     }
+    //     var get = inner.get(size - 1);
+    //     inner.remove(size - 1);
+    //     return get;
+    // }
 
-    private static Integer get_or_zero(ArrayList<Integer> inner, int index){
-        try {
-            return inner.get(index);
-        } catch (Exception e) {
-            return 0;
-        }
-    }
-
+    // private static Integer get_or_zero(ArrayList<Integer> inner, int index){
+    //     try {
+    //         return inner.get(index);
+    //     } catch (Exception e) {
+    //         return 0;
+    //     }
+    // }
+    
+    ///This gets the value in the index as a str, but if it cannot be accessed we return a space str.
     public static String get_as_str_or_space(ArrayList<Integer> inner, int index){
         try {
             return inner.get(index).toString();
@@ -163,6 +189,8 @@ public class Towers {
     //Constructors
 
     public Towers() {
+        ///We create the left, middle, and right pegs.
+        ///Funny hint: with_num(0) simply returns an empty peg. LOL.
         left = with_num(5);
         middle = with_num(0);
         right = with_num(0);
