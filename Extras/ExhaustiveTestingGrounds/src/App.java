c467ab1b887771ab2ba65af6fce9c60bfc62166e
PulsarProgrammer <programingcoder101@gmail.com>
2024-03-15 13:00:38 -0400

    Nice push

--- Extras/ExhaustiveTestingGrounds/src/App.java
+++ Extras/ExhaustiveTestingGrounds/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println(alphaSort("hello monkie "));
         System.out.println("Hello, World!");
     }
+
+
+
+
+
+
+
+    public static String alphaSort(String str) {
+        char[] array = str.toCharArray();
+        Character[] charObjectArray = str.chars().mapToObj(c -> (char)c).toArray(Character[]::new);
+        Arrays.sort(charObjectArray);
+        String growable = "";
+        for(var i = 0; i < array.length; i++){
+            growable += charObjectArray[i];
+        }
+        return growable;
+    }	
 }
