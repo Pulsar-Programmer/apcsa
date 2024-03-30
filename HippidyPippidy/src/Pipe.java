54bbd411914af811599ff18287db9dc7e16d6b80
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-03-30 15:34:43 -0400

    Super senior!!!

--- HippidyPippidy/src/Pipe.java
+++ HippidyPippidy/src/Pipe.java
@@ -42,5 +42,108 @@ public class Pipe {
         return board.isEmpty();
     }
 
+    public ArrayList<ArrayList<Character>> start(){
+        var points = new ArrayList<Point>();
+        var solution = new ArrayList<ArrayList<Character>>();
+        for(var i = 0; i < board.size(); i++){
+            var row = new ArrayList<Character>();
+            for(var j = 0; j < board.get(i).size(); j++){
+                row.add(Character.valueOf(board.get(i).get(j)));
+            }
+            solution.add(row);
+        }
+        move(start, points);
+        for(var i = 0; i < points.size(); i+=1){
+            var p = points.get(i);
+            solution.get(p.y).set(p.x, '.');
+        }
+
+        return solution;
+    }
+    public boolean move(Point position, ArrayList<Point> points){
+        if(position.equals(end)){
+            return true;
+        }
+        try_place(position);
+        switch(try_get(position)){
+            case 'x': case '.' : break;
+            case '|' : {
+                if(try_vertical(position, points)){return true;}
+            } break;
+            case '-' : {
+                if(try_horizontal(position, points)){return true;}
+            } break;
+            case 'A': {
+                if(try_vertical(position, points)){return true;}
+                if(try_horizontal(position, points)){return true;}
+            } break;
+            default : break;
+        }
+        return false;
+    }
+
+    private boolean try_vertical(Point position, ArrayList<Point> points){
+        final var above = flank_above(position);
+        final var below = flank_below(position);
+        if(move(above, points)) { points.add(above); return true; }
+        if(move(below, points)) { points.add(below); return true; }
+        return false;
+    }
+    private boolean try_horizontal(Point position, ArrayList<Point> points){
+        final var left = flank_left(position);
+        final var right = flank_right(position);
+        if(move(left, points)) { points.add(left); return true; }
+        if(move(right, points)) { points.add(right); return true; }
+        return false;
+    }
+
+    private static Point flank_below(Point p){
+        Point point = (Point)p.clone();
+        point.y -= 1;
+        return point;
+    }
+    private static Point flank_above(Point p){
+        Point point = (Point)p.clone();
+        point.y += 1;
+        return point;
+    }
+    private static Point flank_left(Point p){
+        Point point = (Point)p.clone();
+        point.x -= 1;
+        return point;
+    }
+    private static Point flank_right(Point p){
+        Point point = (Point)p.clone();
+        point.x += 1;
+        return point;
+    }
+
+
+
+    private void try_place(Point p){
+        try {
+            board.get(p.y).set(p.x, '.');
+        } catch (final Exception _e) {}
+    }
+    private Character try_get(Point p){
+        try {
+            return board.get(p.y).get(p.x);
+        } catch (final Exception _e) {
+            return 'x';
+        }
+    }
+
+    @Override
+    public String toString() {
+        var string = new String();
+        for(var i = 0; i < board.size(); i++){
+            for(var j = 0; j < board.get(i).size(); j++){
+                string += board.get(i).get(j);
+            }
+            string += '\n';
+        }
+        return string;
+    }
     
+
 }
--- HippidyPippidy/src/PipeDriver.java
+++ HippidyPippidy/src/PipeDriver.java
@@ -3,11 +3,28 @@ import java.util.Scanner;
 
 public class PipeDriver {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        final var scanner = new Scanner(System.in);
+
+        var scanner = new Scanner(System.in);
         System.out.println("What is the name of the file?");
         final var filename = scanner.nextLine();
+        scanner.close();
         var my_pipe = new Pipe(new File(filename));
+        final var result = my_pipe.start();
+        var found = false;
+        for(var i = 0; i < result.size(); i++){
+            for(var j = 0; j < result.get(i).size(); j++){
+                var c = result.get(i).get(j);
+                if(c=='.'){
+                    found = true;
+                }
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+        if(!found){
+            System.out.println("There is no solution for this board.");
+        }
+        System.out.println(result);
 
     }
 }
