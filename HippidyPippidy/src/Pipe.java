92bb70194a8a7c5ac8cc90fca4845396895953e1
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-04-03 22:36:17 -0400

    Comments!

--- HippidyPippidy/src/Pipe.java
+++ HippidyPippidy/src/Pipe.java
@@ -9,42 +9,60 @@ public class Pipe {
     private Point end;
     private ArrayList<ArrayList<Character>> board;
     
-
+    ///We create our constructor that takes in a file and constructs the class.
     public Pipe(File file) throws Exception{
+        ///We set our start and end to bad points out of bounds. Don't worry- this is not a problem.
+        ///This is because of the way the get method is implemented --- you'll see.
         start = new Point(-1, -1);
         end = new Point(-1, -1);
+        ///We create our new array to house the result of the board.
         ArrayList<ArrayList<Character>> result = new ArrayList<>();
+        ///We scan and keep track of the amount of times scanned.
         final var scanner = new Scanner(file);
         var layer = 0;
         while (scanner.hasNextLine()) {
+            ///We get the line.
             final var line = scanner.nextLine();
             ArrayList<Character> line_list = new ArrayList<>();
             final var array = line.toCharArray();
+            ///We loop over each character and add it accordingly.
             for(var i = 0; i < array.length; i += 1){
                 if('A' == array[i]){
+                    ///Now if the start equals the end (meaning they are both the bad points from earlier), we can safely set start to a value. Start will never equal end otherwise.
                     if(start.equals(end)) {
+                        ///We create start.
                         start = new Point(i, layer);
                     }
                     else{
+                        ///We otherwise create end.
                         end = new Point(i, layer);
                     }
                 }
+                ///We add each character.
                 line_list.add(array[i]);
             }
+            ///We add each line.
             result.add(line_list);
+            ///We increment layer.
             layer += 1;
         }
+        ///We close the scanner and initialize the last var.
         scanner.close();
         board = result;
     }
 
+    ///This checks if the board is empty.
     public boolean is_empty(){
         return board.isEmpty();
     }
 
+    ///This starts the solving process.
     public ArrayList<ArrayList<Character>> start(){
+        ///We create a set of points that we must keep.
         var points = new HashSet<Point>();
+        ///We create our solution to display.
         var solution = new ArrayList<ArrayList<Character>>();
+        ///We create a duplicate array for the displaying.
         for(var i = 0; i < board.size(); i++){
             var row = new ArrayList<Character>();
             for(var j = 0; j < board.get(i).size(); j++){
@@ -52,20 +70,25 @@ public class Pipe {
             }
             solution.add(row);
         }
+        ///We start the moving.
         move(start, points);
+        ///We transfer over the relevant points.
         for(var p : points){
             solution.get(p.y).set(p.x, '.');
         }
-
+        ///We return the solution.
         return solution;
     }
     public boolean move(Point position, HashSet<Point> points){
         // System.out.println("Considering point " + position);
+        ///We check if we are at end yet. If we are, we return true, signifying the ability for the point to be recognized.
         if(position.equals(end)){
             return true;
         }
         // System.out.println(try_get(position));
+        ///We match on the character of the position.
         switch(try_get(position)){
+            ///Nothing happens unless it is a | or - or A whereby verticals, horizontals, and more are considered.
             case 'x': case '.' : break;
             case '|' : {
                 try_place(position);
@@ -81,16 +104,20 @@ public class Pipe {
             } break;
             default : break;
         }
+        ///We return false if no returns could be made.
         return false;
     }
-
+    ///This tries to move above or below.
     private boolean try_vertical(Point position, HashSet<Point> points){
         final var above = flank_above(position);
         final var below = flank_below(position);
+        ///We look above or below. If the move is successful, we backtrack and return.
         if(move(above, points)) { points.add(above); return true; }
         if(move(below, points)) { points.add(below); return true; }
+        ///Otherwise we return false.
         return false;
     }
+    ///Same thing as try_vertical but for horizontal.
     private boolean try_horizontal(Point position, HashSet<Point> points){
         final var left = flank_left(position);
         final var right = flank_right(position);
@@ -98,22 +125,25 @@ public class Pipe {
         if(move(right, points)) { points.add(right); return true; }
         return false;
     }
-
+    ///Creates a point one below the input.
     private static Point flank_below(Point p){
         Point point = (Point)p.clone();
         point.y -= 1;
         return point;
     }
+    ///Creates a point one above the input.
     private static Point flank_above(Point p){
         Point point = (Point)p.clone();
         point.y += 1;
         return point;
     }
+    ///Creates a point one left of the input.
     private static Point flank_left(Point p){
         Point point = (Point)p.clone();
         point.x -= 1;
         return point;
     }
+    ///Creates a point one right of the input.
     private static Point flank_right(Point p){
         Point point = (Point)p.clone();
         point.x += 1;
@@ -121,12 +151,13 @@ public class Pipe {
     }
 
 
-
+    ///Tries to place a dot a p. If it works, yay, but if it doesn't, no problem. Dot and X are considered the same anyway.
     private void try_place(Point p){
         try {
             board.get(p.y).set(p.x, '.');
         } catch (final Exception _e) {}
     }
+    ///We try to get the character at a point p. If it works, then we can return the character otherwise we must return 'x'. This allows no out of bounds sussiness to happen.
     private Character try_get(Point p){
         try {
             return board.get(p.y).get(p.x);
@@ -134,7 +165,7 @@ public class Pipe {
             return 'x';
         }
     }
-
+    ///How we display our board.
     @Override
     public String toString() {
         var string = new String();
@@ -146,7 +177,7 @@ public class Pipe {
         }
         return string;
     }
-
+    ///Standard getters and setters.
     public Point getStart() {
         return start;
     }
--- HippidyPippidy/src/PipeDriver.java
+++ HippidyPippidy/src/PipeDriver.java
@@ -1,41 +1,53 @@
 import java.io.File;
 import java.util.Scanner;
+///Triple comments are intended to be read.
+//Double comments are not intended to be read.
 
 public class PipeDriver {
     public static void main(String[] args) throws Exception {
+        ///We create a scanner and ask some stuff.
         var scanner = new Scanner(System.in);
         while(true){
             System.out.println("What is the name of the file?");
             final var filename = scanner.nextLine();
+            ///We create our pipe.
             var my_pipe = new Pipe(new File(filename));
+            ///We start it and get the end for later.
             final var result = my_pipe.start();
             final var p_end = my_pipe.getEnd();
             System.out.println("Your solution:");
+            ///Found keeps track of whether any '.' were found, denoting a positive or negative solution.
             var found = false;
             for(var i = 0; i < result.size(); i++){
                 for(var j = 0; j < result.get(i).size(); j++){
                     var c = result.get(i).get(j);
+                    ///If we find the dot, which is always present in a solved board (since the end marker (A) is dotted), we can say that it has a solution.
                     if(c=='.'){
                         found = true;
                     }
+                    ///We then print the A character in place of the final '.'
                     if(p_end.y == i && p_end.x == j){
                         System.out.print('A');
                         continue;
                     }
+                    ///We print c at that position for toString();
                     System.out.print(c);
                 }
+                ///We print a line to go to the next line.
                 System.out.println();
             }
+            ///If we didn't find any dots, we can safely deem no solution.
             if(!found){
-                //this doesn't work if the As are next to each other
                 System.out.println("There is no solution for this board.");
             }
+            ///We request another playthrough.
             System.out.println("Would you like to play again? Yes=`true` No=`false`");
             final var bool = scanner.nextBoolean(); scanner.nextLine();
             if(!bool){
                 break;
             }
         }
+        ///We end.
         scanner.close();
     }
 }
