a2958a00f030d898432c62abdcaf514bb791baf3
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-04-25 02:12:04 -0400

    Add comments ! Done!!!

--- FractalTactile/src/App.java
+++ FractalTactile/src/App.java
@@ -6,16 +6,21 @@ import javax.swing.SwingConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+///Triple comments are intended to be read.
+//Double comments are not intended to be read.
+
+
+
 public class App {
-	static int level = 0;
 	public static void main(String[] args) {
-		
+		///The Fractal Maze! We set up the Frame.
 		JFrame mainframe = new JFrame();
-		mainframe.setTitle("The Fractal Circle");
+		mainframe.setTitle("The Fractal Maze");
 		mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainframe.setVisible(true);
         mainframe.setSize(Fractal.x(100), Fractal.y(100) - 30);
-		
+
+		///We set up the slider.
 		JSlider slider = new JSlider(SwingConstants.HORIZONTAL,1,20,3);
 		slider.setMajorTickSpacing(5);
 		slider.setMinorTickSpacing(1);
@@ -23,18 +28,18 @@ public class App {
 		slider.setPaintTicks(true);
 		slider.setPaintLabels(true);
 		
+		///We set up the painting.
 		Fractal painting = new Fractal();
 		mainframe.add(painting, BorderLayout.CENTER);
 		mainframe.setVisible(true);
 		
-		
+		///We set up the event listener which allows it to be repainted each time.
 		//addChangeListener runs stateChanged(ChangeEvent e) whenever changes are made to the slider.
 		slider.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				if(slider.getValue() != level) {
-					level = slider.getValue();
-					painting.setLevel(level);
+				if(slider.getValue() != painting.getLevel()) {
+					painting.setLevel(slider.getValue());
 					mainframe.repaint();
 					painting.getGen().clear();
 				}
--- FractalTactile/src/Fractal.java
+++ FractalTactile/src/Fractal.java
@@ -5,7 +5,6 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.util.HashSet;
-
 import javax.swing.JPanel;
 import java.awt.geom.Line2D;
 
@@ -30,37 +29,50 @@ public class Fractal extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		///We create our Graphics instance.
 		Graphics2D g2d = (Graphics2D)g;
+		///We set the stroke of it with a width of five, and draw a starter circle in the middle.
 		g2d.setStroke(new BasicStroke(5));
 		g2d.fillOval(x(50) - 25, y(50) - 25, 50, 50);
+		///We get the start point and check if we have generation yet.
 		var start = new Point(x(50), y(50));
 		if(gen.isEmpty()){
-			circuit(g2d, start, flank_above(start));
-			circuit(g2d, start, flank_below(start));
-			circuit(g2d, start, flank_left(start));
-			circuit(g2d, start, flank_right(start));
+			///If no generation is found, we start our circuit-chain by recursively calling each method in each direction.
+			circuit(start, flank_above(start));
+			circuit(start, flank_below(start));
+			circuit(start, flank_left(start));
+			circuit(start, flank_right(start));
 		}
+		///Each redrawing, we regenerate all the colors and draw all the generated lines.
 		for (Line2D line2d : gen) {
 			final var color = new Color((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256));
 			g2d.setColor(color);
 			g2d.drawLine((int)line2d.getX1(), (int)line2d.getY1(), (int)line2d.getX2(), (int)line2d.getY2());
 		}
 	}
-	
-	public void circuit(Graphics2D g2d, Point prev, Point curr) {
+	///This is our circuit.
+	public void circuit(Point prev, Point curr) {
+		///We get the previous and current point to draw and make a line.
+		///If the generation is filled, we should stop.
 		if(gen.size() > 1000) return;
+		///Otherwise, lets make a line and check if it is already in the generation.
 		var line = new Line2D.Float(prev, curr);
+		///If the line has already been made, we can remove it.
 		if(gen.contains(line)) return;
+		///If the distance from the origin is great enough, we decrease the odds of developing a new span.
 		if(Math.random() >= dist_from_origin(prev)) return;
 		
-		
+		///We add the generation to the gen since it cleared the tests.
 		gen.add(line);
-		circuit(g2d, curr, flank_above(curr));
-		circuit(g2d, curr, flank_below(curr));
-		circuit(g2d, curr, flank_left(curr));
-		circuit(g2d, curr, flank_right(curr));
+		///Now we continue, drawing our other variants.
+		circuit(curr, flank_above(curr));
+		circuit(curr, flank_below(curr));
+		circuit(curr, flank_left(curr));
+		circuit(curr, flank_right(curr));
 	}
 
+	///This is a probability function that determines the probability dependent on the distance the line is away from the origin.
+	///This is truly fascinating. If you want, you can plug it into desmos and see how it evolves.
 	private double dist_from_origin(Point point){
 		final var dist = Math.pow(Math.pow(point.x - x(50), 2) + Math.pow(point.y - y(50), 2), 0.5);
 		return Math.exp(-0.5 -dist/(-level * 5 + 250.)) - 0.3 * Math.exp(-Math.pow((dist - 110 + 4 * level)/20., 2));
@@ -99,7 +111,7 @@ public class Fractal extends JPanel {
     public static int y(double pct){
         return (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * pct / 100.0);
     }
-
+	
 	public HashSet<Line2D> getGen() {
 		return gen;
 	}
